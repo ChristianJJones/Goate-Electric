332c41d2747fde311bbecec6c2c9eb5d90250f9e
ChristianJJones <christian.j.jones@outlook.com>
2025-04-07 00:45:12 -0500

    Create TheGoateToken.sol

---
+++ contract/TheGoateToken.sol
@@ -0,0 +1,141 @@
+pragma solidity ^0.8;
+
+// The Goate Token, a token that has a DIFFERENT PRICE to everybody depending on how they interact with the NETWORK and their STANDING in it as a (GOOD || BAD) ACTOR
+// when they exchange at other places then there will be a huge "arbitrage" gap; 
+when they buy The Goate Token then they will have atleast that much in $USD to sell with,
+The Goate Token gets airdropped to people based on them hittin achievements. 
+
+// How they interact with the network: 
+1. amount of zeropoint consumed
+2. amount of zeropointWifi consumed
+3. amount of zeropointPhoneService consumed
+4. number of ads watched
+5. number of times staked assets
+6: amount of money staking currently
+7. number of times lent money
+8: amount of money they are lending currently
+9. number of times borrowed assets
+10. number of times they paid back whats borrowed
+11: amount of money borrowing currently
+12: number of times bet in HomeTeamBets
+13: amount of money bet in HomeTeamBets
+14: amount of time they spent playing games of gerastyxOpol
+15: number of times theyve played gerastyxOpol
+16: how many times theyve bought grey stax
+17. how many times theyve sold grey stax
+18. how many grey stax do they have now (balances) and how much in usd ( usd denomination)
+19.: how many times theyve bought the goate token (l) 
+20. how many times theyve sold the goate token (l) 
+21. how many the goate token (l)  do they have now (balances) and how much in usd ( usd denomination)
+22: how many times theyve bought zeropoint (zpe)
+23. how many times theyve sold zeropoint (zpe)
+24. how many zeropoint (zpe) do they have now (balances) and how much in usd ( usd denomination)
+25: how many times theyve bought zeropointWifi (zpw)
+26. how many times theyve sold zeropointWifi (zpw)
+27. how many zeropointWifi (zpw) do they have now (balances) and how much in usd ( usd denomination)
+28: how many times theyve bought zeropointPhoneService (zpp)
+29. how many times theyve sold zeropointPhoneService (zpp)
+30. how many zeropointPhoneService (zpp) do they have now (balances) and how much in usd ( usd denomination)
+
+//their standing as a good or a bad character on a 0 to 10 scale of:
+1. interacting with the network
+2. watching ads / number of ads watched
+3. staking assets
+4. buying The Goate Token (l)
+5. The Goate Token (l) user's balance > $100
+6. consuming zeropoint
+7. consuming zeropointWifi
+8. consuming zeropointPhoneServices
+9. connecting a house on manage devices (instilled interoperability node { node rewards = revenue, connected house gets their house battery connected to 100% battery for free every month.)
+10. connecting a car on manage devices (instilled interoperability node { node rewards = revenue, and people still have to charge their car from the device settings modal in the manage devices page with $ZPE})
+11. kyc/aml (it's required for goate electric but check and store data for all accounts and addresses)
+12. background check, number of convicted felonies
+13. if theyve connected with the taxManager
+
+contract QuantumHackProofStabilizer {
+(require KYC Results); // map people & their accounts throughout the digital metaverse
+(require KYB Results); // direct value & price standings throughout the digital metaverse
+(require credibility score 0-10;)
+
+
+
+function determinePrice (address, token, price){
+(require KYC Results); // map people & their accounts throughout the digital metaverse
+(require KYB Results); // direct value & price standings throughout the digital metaverse
+(require credibility score 0-10)
+// price still depends on marketcap % circulating supply = price 
+// this will make marketcaps avilable out of the single contract address marketcap ... "quantumcaps"
+token contract address = /* here */;
+token contract address[marketcap] = this.marketcap;
+
+msg.sender address = user.address;
+
+msg.sender pioneerRank = {
+(require msg.sender is a KYC Account);
+// factor & weight aggregate consensus on a scale of 1-10
+
+time in network : 5, // date of the first mining session
+efficiency of mining sessions : 5, // how many days mined, how many days minedMissed, percentage
+on/off ratio : 1, // $PI bought, $PI sold, ratio & percentage
+trading activity : 8, // $PI bought[timestamp], $PI sold[timestamp], trading = (yes||no), didPriceInfluxBetweenTransactions = (yes||no), tradingForGains = (yes||no) , didTransactionCausePriceInflux = (yes||no), tradingForMarketManipulation = (yes||no) */ // if trading for market manipulation & arbitrage is found, use: SHA256 override = ChristianJonesIsTheGoate || cj03nesTheGoat, and force transfer(gains), (placeholderForManipulation), and put address & KYC in a delinquincy
+network plug-in usage : 5, // node usage, browser usage, mining frequency, etc ... click&continueTypeOfFunctions
+work in network : 5, // kyc validating, insurance validating, ad-watching, etc ... stop&checkTypeOfFunctions
+// these are only 6 factors but there can be more factors added or add more stuff into each genre
+
+
+so if msg.sender[averageExample] = {
+
+time in network : 250 days out of 5000 possible days;
+efficiency of mining sessions : 112 out of 250 possible mining sessions;
+on/off ratio : 100 $PI worth $170 USD on, 50 $PI worth $85 USD off;
+trading activity : 100 $PI worth $170 USD bought at 10:30 PM on February 20, 2025 at $1.7 USD per $PI, 50 $PI worth $85 USD sold at 10:35 PM on February at $1.7 USD per $PI;
+network plug-in usage : nodeUsage[24 out of 24 hours], piBrowser[5 out of 24 hours], miningFrequency[24 out of 24 hours];
+work in network : kycValidating[363 out of 386 validations], insuranceValidations[100 out of 105 validations], ads[1 out of 1 ads watched]; 
+
+their point would be:
+time in network = 20% out of 100%;
+efficiency of mining sessions : 44% out of 100%;
+on/off ratio : 100 $PI / $170 USD : 50 $PI / $85 USD;
+trading activity : no;
+network plug-in usage : 98% out of 100%;
+work in network : 90% out of 100%;
+}
+
+&& if msg.sender[perfectExample] = {
+
+time in network : 5000 days out of 5000 possible days;
+efficiency of mining sessions : 250 out of 250 possible mining sessions;
+on/off ratio : 100 $PI worth $170 USD on, 0 $PI worth $0 USD off;
+trading activity : 100 $PI worth $170 USD bought at 10:30 PM on February 20, 2025 at $1.7 USD per $PI, 0 $PI sold;
+network plug-in usage : nodeUsage[24 out of 24 hours], piBrowser[24 out of 24 hours], miningFrequency[24 out of 24 hours];
+work in network : kycValidating[386 out of 386 validations], insuranceValidations[105 out of 105 validations], ads[1 out of 1 ads watched]; }
+
+&& so if msg.sender[badExample] = {
+
+time in network : 35 days out of 5000 possible days;
+efficiency of mining sessions : 1 out of 250 possible mining sessions;
+on/off ratio : 100 $PI worth $170 USD on, 100 $PI worth $85000 USD off;
+trading activity : 100 $PI worth $170 USD bought at 10:30 PM on February 20, 2025 at $1.7 USD per $PI, 100 $PI worth $85000 USD sold at 10:35 PM on February at $1.7 USD per $PI;
+network plug-in usage : nodeUsage[0 out of 24 hours], piBrowser[0 out of 24 hours], miningFrequency[4 out of 24 hours];
+work in network : kycValidating[0 out of 0 validations], insuranceValidations[0 out of 0 validations], ads[0 out of 1 ads watched];  
+
+their point would be: }
+
+
+
+
+
+
+   out of 130
+
+
+}
+
+
+
+}
+
+
+
+
+}
