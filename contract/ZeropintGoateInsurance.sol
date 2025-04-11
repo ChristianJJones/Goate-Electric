26f0f579b383d435274328ae2029222d27ca2dd0
ChristianJJones <christian.j.jones@outlook.com>
2025-04-11 18:47:15 -0500

    Create ZeropintGoateInsurance.sol
    Signed-off-by: ChristianJJones <christian.j.jones@outlook.com>

---
+++ contract/ZeropintGoateInsurance.sol
@@ -0,0 +1,66 @@
+// everybody has their own insurance pool based on how many ZeropointGoateInsurance ($ZGI)tokens they consumed to their vehicle, house, motorcycle, boat, aircraft, or their identy in the mange devices section along if theyve connected regular mobile devices like phones , watches, airpods, hearing aids, medical equipment, etc. with ZeropointEnergy($ZPE), ZeropointWifi($ZPW), ZeropointPhoneService($ZPP), ZerpointHolographicView ($ZPV) & S.H.I.E.L.D. Mode ($SDM). Integrate all the new contracts but keep the same kyc,/aml, ui/ux, and USDMediator & instilledIneteroperability databases; just add to it.
+
+
+
+ZeropointGoate Insurance ($ZGI) {
+ZeropointGoateInsurance ($ZGI) = $6 monthly subscription to insurance
+
+
+
+
+when the $5 monthly subscription starts; create a digital card in the ZeropointGoateInsurance Page; kind of like manage devices except its with devices that consumed ZeropointGoateInsurance; 
+Have the ZeropointGoateInsurance Page like:
+{ 
+[description]
+[consume $ZGI button, and a consume $ZGI page that lets them choose how many $ZGI to choose; then once they confirm it, and use their pin then let connect their device to insurance for the time being ]
+[pay by watching ads,once they click and watch the AdWatch google &| youtube &| pi ads 70% revenue to their ZeropointGoateInsurance balance, 30 % to 1. $XLM (15%), 2. $AQUA (20%), 3. $yXLM (20%), 4. $yBTC (15%), 5. $yUSD (15%), 6. ZeropointDigitalStockNFT => Microstrategy (15%) through instilledInteroperability & the USDMediator accounts; ]
+
+[ insured assets manager:
+  [assetInsured information:]
+  [asset holder name:] 
+  [insurance on or off slider (cant be pressed, here or with zeropointgoateinsurance on the manage devices page; when it's manage on the manage devices page too, then it highlights green.]
+
+[make a claim button] - sends a form with their device information on there, their insurance claim balance, and questions: "what happened ?:", "do you have any pictures ?:" and {give them a page to upload pictures, and documents; quantumProof it and add security defenses so any corrupt assets, files, information, uint, or etc data or storage gets siphoned { all Zeropoint Energy dissolved, ZeropointWifi disconnected, ZeropointPhoneService dsconnected, all assets dissolved, and their page refreshed and updated to all their account balances at 0. and they have to start over with a bad standing. },  "how much of YOUR insurance claim do you want:" and let them put a number within their insurances' available balance;   and then at the bottom have a pay with 1 $ZGI button; that button should have a confirm page and then their pin code; once the $ZGI is sent sell it with USDMediator and InstilledInteroperability; and then send it as stakedInsurance to  1. $XLM (15%), 2. $AQUA (20%), 3. $yXLM (20%), 4. $yBTC (15%), 5. $yUSD (15%), 6. ZeropointDigitalStockNFT => Microstrategy (15%);
+] }
+
+their claim information goes to Validation Portal as insuranceClaimInformation: their ID, their face scan while on the form: blur any nudity, and their insuranceClaimInformation; and ask them if that person is was the person on the card; with a yes or no slider that shows yes green and no gray when yes is clicked, and no red with yes gray when no is clicked)
+eash insuranceClaim has to get verified by 4 people before it can released into  your account as $USD through InstilledInteroperability and the USDMediator
+when they pay for their insurance then $0.25 $ZGI goes to each successful validator in validator portal out of 4 ($1 total), and $5 goes to the USDMediator & InstilledInteroperability as stakedInsurance and goes towards 1. $XLM (15%), 2. $AQUA (20%), 3. $yXLM (20%), 4. $yBTC (15%), 5. $yUSD (15%),  ZeropointDigitalStockNFT  6.Microstrategy (5%); 7.Best Buy (5%) 8. Whole Foods (5%) when their device isn't 
+then the money gets deposited into their USD balance when their insurance claim gets atleast 4 yes or 4 no; whichever one hits 4 first is the successful verification;
+
+that's 
+
+ValidationPortal {
+
+  connect them to AdWatch instead of adWatch being its own page still allocate 50% usd to user balance, and 50% to revenue reserve through the USDMediator & InstilledInteroperability
+  a validate identity button: connects them to an insurance claim with less than 4 yes or 4 no ( they get $0.25 to their USD balance for each successful verification)
+  them goldtap button connected to sites to make money and 50% usd is sent to the user, 50% usd is sent to revenue
+  
+}
+
+HolograpchicView ($ZHV) {
+zeropoint + camera( front view) or zeropoint + camera (back view);
+create a 3d effect; make the natural color : blue, but then fill in with a color spectrum to follow videos and play animations; Then connect movies, videos, & TV shows from these sites in modules & the search bar; Holographic View is $2 a month subscription;:
+flixtor.to,
+netflix.com,
+peacock.com,
+disneyplus.com,
+youtube.com,
+hulu.com,
+huluplus.com,
+amazonprime.com,
+
+and make tabs for the 1. Most Watched Videos (recently within 3 months), 2. Newest on Screen, 3. Most Watched Videos (all-time), 3. 
+}
+
+
+S.H.I.E.L.D. Mode ($SDM) {
+
+zeropoint(gun) = icer;
+icer = knockout; 
+knockout = 100 $ZP;
+so $SDM has to equal $1 one dollar; 
+
+the back-camera becomes a lens and the "shoot picture button" turns red and is a "shoot icer" button. When the person clicks the button, then they can shoot their phone as a safe weapon to neuralize someone. put a warning that the police is warned when multiple stots were fired; when you put your phone in $SDM mode then the back camera is a gun and is also recording; when the icer & recording are both playing at a current timestamp time and more than 5 shots were fired then send the recording footage of 30 second before the shot and 30 seconds after to the validationPortal at the same timestamp just 1 day before with the same hours and time of the day; ask them 1. were shots fired at people: yes or no; if yes then 1a, were the police there ?: yes or no, 1b. should the police be called, yes or no; if yes 1c then have thte USDMediator & InstilledInteroperability call the police at 3 stations using the devices connected in the manage devices section; the one close to the gps, the nearest bordering town, and the nearest bordering town east; 30 minutes before the first timestamp before the validationPortal. "Hello we wanted to report an shooting at { gps address } at the { timestamp of the shooting } reported by 4 validators within Goate Electric's Validation Network, the shooter is now { gps of shooter around 30 minutes before the incident (present)(now) , "gps address of accident" is where it will happen; send a team to intercept and defuse the situation. immediately. this is a message from goate electric's securing humanities intelligence, energy, logistics and defenses (S.H.I.E.L.D) for short") then hangup; call all 3 stations at the same  timestamp; different phones at different locations, from the same starting time of 30 minutes before the time of the self-defense mode activation"  "  add a " validate icer self-defense" and when the 4 successful circumstances are found then pay them $0.25 $USD from the revenue reserves with the USDMediator and InstilledInteroperability; ask the participan
+
+}
