b091196587f1526f8c53d347cabcfb1bc1686361
ChristianJJones <christian.j.jones@outlook.com>
2025-04-13 23:55:22 -0500

    Update TheGoateToken.sol
    Signed-off-by: ChristianJJones <christian.j.jones@outlook.com>

--- contract/TheGoateToken.sol
+++ contract/TheGoateToken.sol
@@ -5,35 +5,47 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TheGoateToken is ERC20, Ownable {
-    mapping(address => uint256) public userPrices; // Custom price per user
-    mapping(address => uint256) public achievements; // Track user achievements
+    struct UserActivity {
+        uint256 adsWatched;
+        uint256 energyConsumed;
+        uint256 validations;
+        uint256 stakedAmount;
+        uint256 wifiConsumed;
+        uint256 digitalStockHeld;
+        uint256 betsMade;
+        uint256 gerastyxOpolPlayed;
+        uint256 lotteryTicketsBought;
+        uint256 gamesPlayed;
+    }
 
-    event Airdrop(address indexed recipient, uint256 amount);
+    mapping(address => UserActivity) public userActivities;
+    mapping(address => uint256) public creditScores;
+
+    event ActivityRecorded(address indexed user, string activity, uint256 value);
 
     constructor() ERC20("The Goate Token", "GOATE") Ownable(msg.sender) {
-        _mint(msg.sender, 1000000 * 10**18); // Initial supply: 1M tokens
+        _mint(msg.sender, 1000000 * 10**18);
     }
 
-    // Set custom price for a user
-    function setUserPrice(address user, uint256 price) external onlyOwner {
-        userPrices[user] = price;
+    function recordActivity(address user, string memory activity, uint256 value) external onlyOwner {
+        UserActivity storage ua = userActivities[user];
+        if (keccak256(bytes(activity)) == keccak256(bytes("adsWatched"))) ua.adsWatched += value;
+        else if (keccak256(bytes(activity)) == keccak256(bytes("energyConsumed"))) ua.energyConsumed += value;
+        else if (keccak256(bytes(activity)) == keccak256(bytes("validations"))) ua.validations += value;
+        else if (keccak256(bytes(activity)) == keccak256(bytes("stakedAmount"))) ua.stakedAmount += value;
+        else if (keccak256(bytes(activity)) == keccak256(bytes("wifiConsumed"))) ua.wifiConsumed += value;
+        else if (keccak256(bytes(activity)) == keccak256(bytes("digitalStockHeld"))) ua.digitalStockHeld += value;
+        else if (keccak256(bytes(activity)) == keccak256(bytes("betsMade"))) ua.betsMade += value;
+        else if (keccak256(bytes(activity)) == keccak256(bytes("gerastyxOpolPlayed"))) ua.gerastyxOpolPlayed += value;
+        else if (keccak256(bytes(activity)) == keccak256(bytes("lotteryTicketsBought"))) ua.lotteryTicketsBought += value;
+        else if (keccak256(bytes(activity)) == keccak256(bytes("gamesPlayed"))) ua.gamesPlayed += value;
+        emit ActivityRecorded(user, activity, value);
+        updateCreditScore(user);
     }
 
-    // Get user's custom price
-    function getUserPrice(address user) external view returns (uint256) {
-        return userPrices[user] > 0 ? userPrices[user] : 1 * 10**18; // Default price: 1 token
-    }
-
-    // Record an achievement and airdrop tokens
-    function recordAchievement(address user, uint256 achievementPoints) external onlyOwner {
-        achievements[user] += achievementPoints;
-        uint256 airdropAmount = achievementPoints * 10 * 10**18; // 10 tokens per point
-        _mint(user, airdropAmount);
-        emit Airdrop(user, airdropAmount);
-    }
-
-    // Override transfer to respect custom pricing (optional logic)
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
-        return super.transfer(recipient, amount);
+    function updateCreditScore(address user) internal {
+        UserActivity memory ua = userActivities[user];
+        uint256 score = (ua.adsWatched * 10) + (ua.stakedAmount / 1e18 * 50) + (ua.energyConsumed / 1e3 * 20);
+        creditScores[user] = score > 1000 ? 1000 : score; // Cap at 1000
     }
 }
