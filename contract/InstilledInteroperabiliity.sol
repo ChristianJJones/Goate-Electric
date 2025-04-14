dcabf6a7cc29b6c42b8b71fa033fa4485381029a
ChristianJJones <christian.j.jones@outlook.com>
2025-04-14 00:40:10 -0500

    Update InstilledInteroperabiliity.sol
    Signed-off-by: ChristianJJones <christian.j.jones@outlook.com>

--- contract/InstilledInteroperabiliity.sol
+++ contract/InstilledInteroperabiliity.sol
@@ -19,15 +19,15 @@ contract InstilledInteroperability {
     constructor(address _goateToken) {
         owner = msg.sender;
         goateToken = TheGoateToken(_goateToken);
-        // Token mappings from provided version
-        tokenMap[1]["USDC"] = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // Ethereum USDC
-        tokenMap[1]["ZPE"] = 0xYourZPEAddress; // Placeholder
-        tokenMap[1]["ZPW"] = 0xYourZPWAddress; // Placeholder
-        tokenMap[1]["ZPP"] = 0xYourZPPAddress; // Placeholder
-        tokenMap[1]["GySt"] = 0xYourGreyStaxAddress; // Placeholder
-        tokenMap[512]["XLM"] = 0xYourStellarXLMAddress; // Placeholder
-        tokenMap[1]["GOATE"] = _goateToken; // Added Goate Token
-        // Trading APIs from provided version
+        // Token mappings
+        tokenMap[1]["USDC"] = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+        tokenMap[1]["ZPE"] = 0xYourZPEAddress;
+        tokenMap[1]["ZPW"] = 0xYourZPWAddress;
+        tokenMap[1]["ZPP"] = 0xYourZPPAddress;
+        tokenMap[1]["GySt"] = 0xYourGreyStaxAddress;
+        tokenMap[512]["XLM"] = 0xYourStellarXLMAddress;
+        tokenMap[1]["GOATE"] = _goateToken;
+        // Trading APIs
         tradingAPIs["Alpaca"] = "https://api.alpaca.markets";
         tradingAPIs["Tradier"] = "https://api.tradier.com";
         tradingAPIs["SnapTrade"] = "https://api.snaptrade.com";
@@ -47,25 +47,21 @@ contract InstilledInteroperability {
         sportsDataAPIs["SportsRadar"] = "https://api.sportsradar.com";
     }
 
-    // Add or update a token in the map
     function setToken(uint256 chainId, string memory symbol, address tokenAddress) external onlyOwner {
         tokenMap[chainId][symbol] = tokenAddress;
     }
 
-    // Get sports data API endpoint
     function getSportsDataAPI(string memory apiName) external view returns (string memory) {
         return sportsDataAPIs[apiName];
     }
 
-    // Cross-chain transfer from provided version
     function crossChainTransfer(uint256 fromChain, uint256 toChain, string memory tokenSymbol, uint256 amount, address recipient) external {
         IERC20(tokenMap[fromChain][tokenSymbol]).transferFrom(msg.sender, address(this), amount);
         emit CrossChainTransfer(fromChain, toChain, tokenSymbol, amount, recipient);
     }
 
-    // Mediator account from provided version
     function mediatorAccount() external view returns (address) {
-        return 0xYourMediatorAccount; // Placeholder
+        return 0xYourMediatorAccount;
     }
 
     event CrossChainTransfer(uint256 fromChain, uint256 toChain, string tokenSymbol, uint256 amount, address recipient);
